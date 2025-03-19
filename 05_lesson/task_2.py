f646c917b9afdc7982143c0a708429d3aff30218
ayoshin82 <ayoshin82@.gmail.com>
2025-03-19 07:09:42 +0700

    Commit Hw5

--- 05_lesson/task_2.py
+++ 05_lesson/task_2.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager
+                                                ().install()))
 
 
 driver.maximize_window()
--- 05_lesson/task_3.py
+++ 05_lesson/task_3.py
@@ -6,7 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager
+                                                ().install()))
 
 driver.maximize_window()
 driver.get('http://uitestingplayground.com/classattr')
@@ -16,6 +17,6 @@ for i in range(3):
     alert = driver.switch_to.alert
     alert.accept()
     driver.refresh()
-    
+
 sleep(10)
 driver.quit()
